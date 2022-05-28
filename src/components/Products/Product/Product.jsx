581d29ed8c59f2c122b07cecaf43e52ed7df2b2d
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-05-28 23:57:47 +0530

    data fetching done

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+import useStyles from './styles'
+
+const Product = ({ product }) => {
+    const classes = useStyles()
+
+    console.log(product)
+
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.image.url} title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5'>
+                        {product.price.formatted_with_symbol}
+                    </Typography>
+                </div>
+                {/* you can use dangerouslySetInnerHTML={{__html: product.description}} instead of slice method*/}
+                <Typography variant='body2' color='textSecondary'>{product.description.slice(3, -4)}</Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label='Add to Card'>
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default Product
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from "@chec/commerce.js"
+
+export const commerce = new Commerce(process.env.REACT_APP_WEB_STORE_PUBLIC_KEY, true)
