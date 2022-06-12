d403cc98a3b28ccd63f79299dac712541cd90ae5
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-06-12 07:47:46 +0530

    key added

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,6 @@
 import Commerce from "@chec/commerce.js"
 
-export const commerce = new Commerce(process.env.REACT_APP_WEB_STORE_PUBLIC_KEY, true)
+// export const commerce = new Commerce(process.env.REACT_APP_WEB_STORE_PUBLIC_KEY, true)
+
+
+export const commerce = new Commerce('pk_436252dc236b4e7f18ff4cb9c81ac3cdea8538f573913', true)
